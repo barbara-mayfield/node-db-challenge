2128dde39bc99c409e07a8672d32809a6cd01a0d
Barbara Mayfield <bmayfield1991@gmail.com>
2020-01-10 18:43:54 -0600

    Initial migration wrote and ran

--- migrations/20200110183101_initial.js
+++ migrations/20200110183101_initial.js
@@ -1,8 +1,40 @@
+exports.up = async function(knex) {
+  await knex.schema.createTable("projects", (table) => {
+      table.increments("id")
+      table.string("name").notNull()
+  })
 
-exports.up = function(knex) {
-  
+  await knex.schema.createTable("tasks", (table) => {
+      table.increments("id")
+      table.string("task_name").notNull()
+      table.boolean("completed")
+  })
+
+  await knex.schema.createTable("resources", (table) => {
+      table.increments("id")
+      table.string("name").notNull()
+  })
+
+  await knex.schema.createTable("project_tasks", (table) => {
+      table.integer("project_id")
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE")
+      table.integer("task_id")
+        .notNullable()
+        .references("id")
+        .inTable("tasks")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE")  
+      table.primary(["project_id", "task_id"])
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("project_tasks")
+  await knex.schema.dropTableIfExists("resources")
+  await knex.schema.dropTableIfExists("tasks")
+  await knex.schema.dropTableIfExists("projects") 
 };
