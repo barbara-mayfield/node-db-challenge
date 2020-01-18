d5e97e4255ac6ee3ffb1d228d85ce88872b95756
Barbara Mayfield <bmayfield1991@gmail.com>
2020-01-17 20:47:09 -0600

    Get tasks accomplish MVP requirement

--- projects/task-model.js
+++ projects/task-model.js
@@ -1,10 +1,12 @@
 const db = require("../data/dbConfig")
 
-function getTasks(id){
-    // SELECT * FROM tasks AS t
-    // JOIN projects AS p ON p.id = t.project_id;
-    return db("tasks as t")
-        .join("projects as p")
+function getTasks(){
+    // SELECT p.id, p.name, p.description, t.name AS task_name FROM projects AS p
+    // JOIN projects_tasks AS pt
+    // JOIN tasks as t ON pt.task_id = t.id;
+    return db("projects as p")
+        .join("projects_tasks as pt")
+        .join("tasks as t")
         .select("p.id", "p.name", "p.description", "t.name as task_name")
 } 
 
