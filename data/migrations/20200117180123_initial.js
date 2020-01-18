bcc8545454b2814ec501a88b4e794cd575276c64
Barbara Mayfield <bmayfield1991@gmail.com>
2020-01-17 18:09:05 -0600

    Re-cloned for a fresh start

---
+++ data/migrations/20200117180123_initial.js
@@ -0,0 +1,39 @@
+exports.up = async function(knex) {
+    await knex.schema.createTable("projects", (table) => {
+        table.increments("id")
+        table.string("name").notNullable()
+        table.string("description")
+        table.boolean("completed").defaultTo(false).notNullable()
+    })
+  
+    await knex.schema.createTable("tasks", (table) => {
+        table.increments("id")
+        table.string("name").notNullable()
+        table.boolean("completed").defaultTo(false).notNullable()
+
+        table.integer("project_id")
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE")  
+    })
+  
+    await knex.schema.createTable("resources", (table) => {
+        table.increments("id")
+        table.string("resource").notNull()
+  
+        table.integer("project_id")
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE")
+    })
+}
+  
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists("resources")
+    await knex.schema.dropTableIfExists("tasks")
+    await knex.schema.dropTableIfExists("projects") 
+};
