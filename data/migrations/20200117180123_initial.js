198c414277f47403cebbdc79b661b1ce8882132e
Barbara Mayfield <bmayfield1991@gmail.com>
2020-01-17 20:34:15 -0600

    added project_tasks table and seed file

--- data/migrations/20200117180123_initial.js
+++ data/migrations/20200117180123_initial.js
@@ -30,9 +30,25 @@ exports.up = async function(knex) {
         .onDelete("CASCADE")
         .onUpdate("CASCADE")
     })
+
+    await knex.schema.createTable("projects_tasks", (table) => {
+        table.integer("project_id")
+            .notNullable()
+            .references("id")
+            .inTable("projects")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE")
+        table.integer("task_id")
+            .notNullable()
+            .references("id")
+            .inTable("tasks")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE")
+    })
 }
   
 exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists("projects_tasks")
     await knex.schema.dropTableIfExists("resources")
     await knex.schema.dropTableIfExists("tasks")
     await knex.schema.dropTableIfExists("projects") 
