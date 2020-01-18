fb135bc3f13ac1ae2aee47c3b5e7a35fe2c761c8
Barbara Mayfield <bmayfield1991@gmail.com>
2020-01-17 18:44:37 -0600

    Seed files ran

--- data/seeds/02-tasks.js
+++ data/seeds/02-tasks.js
@@ -1,8 +1,8 @@
 exports.seed = async (knex) => {
   await knex("tasks").insert([
-    { task_name: "Make your egg container", project_id: 1 },
-    { task_name: "Test your project!", project_id: 1 },
-    { task_name: "Gather all materials for volcano", project_id: 2 },
-    { task_name: "Build your volcano", project_id: 2 },
+    { name: "Make your egg container", project_id: 1 },
+    { name: "Test your project!", project_id: 1 },
+    { name: "Gather all materials for volcano", project_id: 2 },
+    { name: "Build your volcano", project_id: 2 },
   ])
 }
