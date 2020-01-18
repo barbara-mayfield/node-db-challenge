3c54970e9ba63d6548d30e239df154aa52d21174
Barbara Mayfield <bmayfield1991@gmail.com>
2020-01-17 19:23:50 -0600

    getProjects, getProject, addProject working

--- projects/project-router.js
+++ projects/project-router.js
@@ -8,13 +8,32 @@ const router = express.Router()
 router.use('/tasks', taskRouter)
 router.use('/resources/:id', resourceRouter)
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try{
-    projectModel.getProjects()
-    res.json(project);
+    const projects = await projectModel.getProjects()
+    res.json(projects);
   } catch(err) {
     next(err)
   };
 });
+
+router.get('/:id', async(req, res, next) => {
+    try {
+        const { id } = req.params
+        const project = await projectModel.getProject(id)
+        res.json(project)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post("/", async (req, res, next) => {
+    try {
+      const newProject = await projectModel.addProject(req.body)
+      res.status(201).json(newProject)
+    } catch(err) {
+      next(err)
+    }
+  })
 
 module.exports = router;
